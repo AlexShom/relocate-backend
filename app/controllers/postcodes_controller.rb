ac805af20e57164da4b057ab0ad6369dd983dc04
AlexShom <a.shomalistos@gmail.com>
2020-01-16 16:51:18 +0000

    sanitised values from string/integer combinations to all integers on postcodes from json

--- app/controllers/postcodes_controller.rb
+++ app/controllers/postcodes_controller.rb
@@ -2,10 +2,12 @@ class PostcodesController < ApplicationController
   def index
     postcodes = Postcode.all.map { |code| { 
       postcode: code.data["Postcode"],
-      ave_rent: code.data["Average rent"],
-      ave_price: code.data["Average £/sqft"],
+      ave_rent: code.data["Average rent"].to_s.gsub(",", "").to_i,
+      ave_price: code.data["Average £/sqft"].to_s.gsub(",", "").to_i,
       ave_yield: code.data["Average yield"]
       } }
+      
+      
 
     render json: postcodes
   end
