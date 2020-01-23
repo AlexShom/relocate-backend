6c9ff238f44cb80b6974f81c685287e78a23d797
AlexShom <a.shomalistos@gmail.com>
2020-01-23 14:42:24 +0000

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://relocate-frontend.netlify.com/"
+    origins "https://relocate-frontend.netlify.com/*"
 
     resource "*",
       headers: :any,
